b28830d17b6b3825e33ee453d5de8b52e8ee2473
max <chennancd@gmail.com>
2021-11-24 23:25:08 +0800

    change log parent location

--- log4j1/src/main/java/Log4jApp.java
+++ log4j1/src/main/java/Log4jApp.java
@@ -2,11 +2,13 @@ import org.apache.log4j.Logger;
 
 public class Log4jApp {
 
-    static Logger log = Logger.getLogger(Log4jApp.class);
+    //static Logger log = Logger.getLogger(Log4jApp.class);
     public static void main(String[] args) {
+        Logger log = Logger.getLogger(Log4jApp.class);
         log.debug("Debug");
         log.info("Info");
         log.error("Error");
+        System.out.println("complete");
     }
 
 }
